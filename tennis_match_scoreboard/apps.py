b1668e48ac0211f344d44adca08b4f60a7bd98d4
khudopaj_ma <a.khudopaj@rostvert.ru>
2026-06-05 15:50:30 +0300

    feat: create Players and Matches models

---
+++ tennis_match_scoreboard/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TennisMatchScoreboardConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'tennis_match_scoreboard'
---
+++ tennis_match_scoreboard/models.py
@@ -0,0 +1,38 @@
+import uuid
+
+from django.db import models
+from django.db.models import F, Q
+
+
+class Players(models.Model):
+    id = models.BigAutoField("ID", primary_key=True)
+    name = models.CharField("Name", max_length=100)
+
+    class Meta:
+        db_table = "Players"
+
+
+class Matches(models.Model):
+    id = models.BigAutoField("ID", primary_key=True)
+    uuid = models.UUIDField(
+        "UUID",
+        default=uuid.uuid4,
+        editable=False
+        )
+    player1 = models.ForeignKey(Players, verbose_name="Player1", on_delete=models.CASCADE, related_name="player1")
+    player2 = models.ForeignKey(Players, verbose_name="Player2", on_delete=models.CASCADE, related_name="player2")
+    winner = models.ForeignKey(Players, verbose_name="Winner", on_delete=models.CASCADE)
+    score = models.CharField("Score")
+
+    class Meta:
+        db_table = "Matches"
+        constraints = [
+            models.CheckConstraint(
+                check=~Q(player1=F("player2")),
+                name="player1_not_player2"
+            ),
+            models.CheckConstraint(
+                check=Q(winner=F("player1")) | Q(winner=F("player2")),
+                name="winner_must_be_player1_or_player2"
+            ),
+        ]
