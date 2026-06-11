83efbd89c7850a259f8bc0fb19eb2225e8fb3e9a
khudopaj_ma <a.khudopaj@rostvert.ru>
2026-06-11 15:37:52 +0300

    feat: add check player exist in form

--- tennis_match_scoreboard/forms.py
+++ tennis_match_scoreboard/forms.py
@@ -1,12 +1,20 @@
 from django import forms
-
+from .models import Players
 
 class NewMatchForm(forms.Form):
     player1 = forms.CharField(max_length=100, required=True)
     player2 = forms.CharField(max_length=100, required=True)
 
     def clean_player1(self):
-        raise forms.ValidationError("Player 1 don't exist")
+        name = self.cleaned_data['player1']
+        try:
+            Players.objects.get(**{"name": name})
+        except Players.DoesNotExist:
+            raise forms.ValidationError("Player one doesn't exist")
 
     def clean_player2(self):
-        raise forms.ValidationError("Player 2 don't exist")
+        name = self.cleaned_data['player2']
+        try:
+            Players.objects.get(**{"name": name})
+        except Players.DoesNotExist:
+            raise forms.ValidationError("Player two doesn't exist")
