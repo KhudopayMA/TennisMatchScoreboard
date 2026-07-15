b3a64602b9dca1f3be5796defafe1e4dbad1f9c5
OldSnakeEater <mihail.hudopay@mail.ru>
2026-07-15 22:04:38 +0300

    style: change import style in __init__.py file in dtos package

--- tennis_match_scoreboard/dtos/__init__.py
+++ tennis_match_scoreboard/dtos/__init__.py
@@ -1,4 +1,6 @@
-from .score_dtos import GameDto, MatchScoreDto, PlayerScoreDto
+from .score_dtos import GameDto as GameDto
+from .score_dtos import MatchScoreDto as MatchScoreDto
+from .score_dtos import PlayerScoreDto as PlayerScoreDto
 
 __all__ = [
     "PlayerScoreDto",
