85a8ec0973caef0e541e3c71313b386951c56c98
OldSnakeEater <mihail.hudopay@mail.ru>
2026-07-21 19:16:37 +0300

    style: change import style

--- tennis_match_scoreboard/dtos/__init__.py
+++ tennis_match_scoreboard/dtos/__init__.py
@@ -1,9 +1,3 @@
 from .score_dtos import GameDto as GameDto
 from .score_dtos import MatchScoreDto as MatchScoreDto
 from .score_dtos import PlayerScoreDto as PlayerScoreDto
-
-__all__ = [
-    "PlayerScoreDto",
-    "MatchScoreDto",
-    "GameDto",
-]
